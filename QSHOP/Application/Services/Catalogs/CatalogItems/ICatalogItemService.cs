5238c6f4814aa37c03a3df61ee91e04cfc31612c
moohamad1378 <moohamad.maaniat.mm@gmail.com>
2022-03-01 00:42:53 +0330

    Some Changes

--- QSHOP/Application/Services/Catalogs/CatalogItems/ICatalogItemService.cs
+++ QSHOP/Application/Services/Catalogs/CatalogItems/ICatalogItemService.cs
@@ -413,6 +413,7 @@ namespace Application.Services.Catalogs.CatalogItems
     }
     public class CatlogPLPRequestDto
     {
+        public int PageIndex { get; set; }
         public int page { get; set; } = 1;
         public int pageSize { get; set; } = 2;
         public int? CatalogTypeId { get; set; }
